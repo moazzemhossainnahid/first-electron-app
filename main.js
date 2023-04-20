f6e29515f1a99eb51d30149ef602c62b5071155f
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-20 21:57:19 +0600

    add app setup

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const { BrowserWindow, app } = require("electron");
 
-
-
-app.whenReady().then(() => {
+const createWindow = () => {
     const mainWindow = new BrowserWindow();
     mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+
+app.on("window-all-closed", () => {
+    // app.quit();
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 })
